4181694f80c7ff8edea3c0f71e1ef78c950f1be8
Aurelio Merenda <aurelio.merenda@docomodigital.com>
2018-10-26 15:34:58 +0200

    Updated filter test

--- src/filter/filter.test.js
+++ src/filter/filter.test.js
@@ -1,12 +1,12 @@
 import filter from './filter';
 
-test('fixed works correctly with one value', () => {
+test('filter works correctly with one value', () => {
     const value = 'drop-shadow(1px 0px 3px rgba(0,0,0,0.3))';
     const result = filter(value);
     expect(result).toBe(value);
 });
 
-test('fixed works correctly without fixed prop', () => {
-    const result = fixed();
+test('filter works correctly without filter prop', () => {
+    const result = filter();
     expect(result).toBeNull();
 });
