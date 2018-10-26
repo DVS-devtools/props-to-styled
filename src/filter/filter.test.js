dcc93374383af649e902e0c77dc6540a357f9ce0
Aurelio Merenda <aurelio.merenda@docomodigital.com>
2018-10-26 16:34:31 +0200

    Fix filter test

--- src/filter/filter.test.js
+++ src/filter/filter.test.js
@@ -2,11 +2,11 @@ import filter from './filter';
 
 test('filter works correctly with one value', () => {
     const value = 'drop-shadow(1px 0px 3px rgba(0,0,0,0.3))';
-    const result = filter(value);
-    expect(result).toBe(value);
+    const result = filter({ filter: value });
+    expect(result).toBe(`filter: ${value}; -webkit-filter: ${value};`);
 });
 
 test('filter works correctly without filter prop', () => {
-    const result = filter();
+    const result = filter({});
     expect(result).toBeNull();
 });
